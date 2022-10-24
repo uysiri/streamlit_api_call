59eeede336f866af16da3319c46489dc60122562
uysiri <109245460+uysiri@users.noreply.github.com>
2022-10-24 13:47:43 -0400

    Update api_call_streamlit.py

--- api_call_streamlit.py
+++ api_call_streamlit.py
@@ -14,14 +14,33 @@ st.markdown("Input your domain name")
 st.info("For example, if your domain name is pspost.co, enter 'pspost.co' into the box.")
 domain = st.text_input('Domain')
 
-for input in domain:
-    try:
-        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both'
-        x = requests.get(url).json()
-        xx = x['body']
-    except NameError:
-        pass
+# for input in domain:
+#     try:
+#         url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both'
+#         x = requests.get(url).json()
+#         xx = x['body']
+#     except NameError:
+#         pass
         
+
+# @st.experimental_memo
+# @st.cache
+# def reemovNestings(xx):
+#     for i in xx:
+#         if type(i) == list:
+#             reemovNestings(i)
+
+# xx = reemovNestings(xx)
+# data = xx[0]
+
+# @st.cache
+# @st.experimental_memo
+# def get_data(data):
+#         dt = requests.get(data).content
+# #         df = pd.read_csv(io.StringIO(dt.decode('utf-8')))
+#         return dt
+# get_data(data)    
+# df = pd.read_csv(io.StringIO(dt.decode('utf-8')))
 
 @st.experimental_memo
 @st.cache
@@ -30,17 +49,29 @@ def reemovNestings(xx):
         if type(i) == list:
             reemovNestings(i)
 
-xx = reemovNestings(xx)
-data = xx[0]
+for input in domain:
+    try:
+        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both'
+        x = requests.get(url).json()
+    except NameError:
+        pass
+
+if 'body' in x:
+    xx=x['body']
+    xx = reemovNestings(xx)
+    data = xx[0]
+else:
+    print('There is no body in x')
+        
 
 @st.cache
 @st.experimental_memo
 def get_data(data):
         dt = requests.get(data).content
-#         df = pd.read_csv(io.StringIO(dt.decode('utf-8')))
-        return dt
-get_data(data)    
-df = pd.read_csv(io.StringIO(dt.decode('utf-8')))
+        df = pd.read_csv(io.StringIO(dt.decode('utf-8')))
+        return df
+    
+df = get_data(data)
     
 st.dataframe(df)
     
