07f78320dd8c7513360a007f5aced98bc2dc0616
uysiri <109245460+uysiri@users.noreply.github.com>
2022-11-08 18:24:18 -0500

    Update api_call_streamlit.py

--- api_call_streamlit.py
+++ api_call_streamlit.py
@@ -33,6 +33,21 @@ domain = st.text_input('Domain')
 limit = st.radio(
     "What is the maximum number of ranked keywords you want to return?",
     ('10', '100', '1000'))
+# if domain and limit == '10':
+#     try:
+#         url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=10'
+#         x = requests.get(url).json()
+#         xx=x['body']
+#         data = xx[0]
+#         df = get_data(data)
+#         df.fillna(0, inplace=True)
+#         for column in df.columns:
+#             if df[column].dtype == 'float64':
+#                 df[column] = df[column].astype(int)
+#         st.write(len(df)-1)
+#         st.dataframe(df)
+#         csv = convert_df(df)
+#         st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
 if domain and limit == '10':
     try:
         url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=10'
@@ -48,33 +63,18 @@ if domain and limit == '10':
         st.dataframe(df)
         csv = convert_df(df)
         st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
-if domain and limit == '100':
-    try:
-        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=100'
-        x = requests.get(url).json()
-        xx=x['body']
-        data = xx[0]
-        df = get_data(data)
-        df.fillna(0, inplace=True)
-        for column in df.columns:
-            if df[column].dtype == 'float64':
-                df[column] = df[column].astype(int)
-        st.write(len(df)-1)
-        st.dataframe(df)
-        csv = convert_df(df)
-        st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
-elif domain and limit == '1000':
-    try:
-        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=1000'
-        x = requests.get(url).json()
-        xx=x['body']
-        data = xx[0]
-        df = get_data(data)
-        df.fillna(0, inplace=True)
-        for column in df.columns:
-            if df[column].dtype == 'float64':
-                df[column] = df[column].astype(int)
-        st.write(len(df)-1)
-        st.dataframe(df)
-        csv = convert_df(df)
-        st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
+    elif domain and limit == '100':
+        try:
+            url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=100'
+            x = requests.get(url).json()
+            xx=x['body']
+            data = xx[0]
+            df = get_data(data)
+            df.fillna(0, inplace=True)
+            for column in df.columns:
+                if df[column].dtype == 'float64':
+                    df[column] = df[column].astype(int)
+            st.write(len(df)-1)
+            st.dataframe(df)
+            csv = convert_df(df)
+            st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
