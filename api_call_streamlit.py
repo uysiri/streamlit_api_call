313712980a2745026b131d9c64d1bd7fb9ba4e3e
uysiri <109245460+uysiri@users.noreply.github.com>
2022-11-08 18:20:39 -0500

    Update api_call_streamlit.py

--- api_call_streamlit.py
+++ api_call_streamlit.py
@@ -48,5 +48,35 @@ if domain and limit == '10':
         st.dataframe(df)
         csv = convert_df(df)
         st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
+elif domain and limit == '100':
+    try:
+        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=100'
+        x = requests.get(url).json()
+        xx=x['body']
+        data = xx[0]
+        df = get_data(data)
+        df.fillna(0, inplace=True)
+        for column in df.columns:
+            if df[column].dtype == 'float64':
+                df[column] = df[column].astype(int)
+        st.write(len(df)-1)
+        st.dataframe(df)
+        csv = convert_df(df)
+        st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
+elif domain and limit == '1000':
+    try:
+        url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=1000'
+        x = requests.get(url).json()
+        xx=x['body']
+        data = xx[0]
+        df = get_data(data)
+        df.fillna(0, inplace=True)
+        for column in df.columns:
+            if df[column].dtype == 'float64':
+                df[column] = df[column].astype(int)
+        st.write(len(df)-1)
+        st.dataframe(df)
+        csv = convert_df(df)
+        st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
     except NameError:
         pass
