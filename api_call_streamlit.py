aa20555d8c21639b75423467bd49e863a48a6f77
uysiri <109245460+uysiri@users.noreply.github.com>
2022-11-11 22:38:11 -0500

    Update api_call_streamlit.py

--- api_call_streamlit.py
+++ api_call_streamlit.py
@@ -28,32 +28,11 @@ def get_data(data):
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-    
-domain = st.text_input('Domain')
 limit = st.radio(
     "What is the maximum number of ranked keywords you want to return?",
-    ('10', '100', '1000'))
-# if domain:
-#     try:
-#         url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=100'
-#         x = requests.get(url).json()
-#         xx=x['body']
-#         data = xx[0]
-#         w = xx[1]
-#         string = ",".join(str(e) for e in w)
-#         newstr = string[9:]
-#         df = get_data(data)
-#         df.fillna(0, inplace=True)
-#         for column in df.columns:
-#             if df[column].dtype == 'float64':
-#                 df[column] = df[column].astype(int)
-#         st.write("Number of Ranked Keywords Returned:",len(df)-1)
-#         st.write("Cost of API Call:",newstr.split(',', 1)[0])
-#         st.dataframe(df)
-#         csv = convert_df(df)
-#         st.download_button(label="Download data as CSV", data=csv, file_name='sample_df.csv', mime='text/csv',)
-#     except NameError:
-#         pass
+    ('10', '100', '1000'))    
+domain = st.text_input('Domain')
+
 if (domain) and (limit == '10'):
     url = 'https://b3z7u9yhxl.execute-api.us-east-1.amazonaws.com/dev/keywords/ranked?domain=' + domain + '&format=both&limit=10'
     x = requests.get(url).json()
